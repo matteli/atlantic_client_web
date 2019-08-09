17ed1d7cd19733c432f9d12004bd9c7bfdadcfdd
matteli <matthieu.nue@gmail.com>
2019-08-09 16:08:52 +0200

    Finish card home without image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,16 @@
     <img src="@/assets/img/Breguet_Atlantic.jpg" alt="Breguet Atlantic" width="100%" />
     <h1 class="text-center bg-primary">Bienvenue sur Atlantic</h1>
     <div>
-      <b-card-group deck v-bind:title="plane.registration"></b-card-group>
+      <b-card-group deck>
+        <b-card
+          v-for="plane in planes"
+          v-bind:key="plane.registration"
+          v-bind:title="plane.registration"
+        >
+          <b-card-text>{{ plane.manufacturer }} {{ plane.model }} ({{ plane.msn }})</b-card-text>
+          <b-button v-bind:href="'/#/log3s/' + plane.registration">3D</b-button>
+        </b-card>
+      </b-card-group>
     </div>
   </div>
 </template>
