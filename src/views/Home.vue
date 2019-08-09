bc9e70a194ff5caea7217c30f36c12cbb2ffb07d
matteli <matthieu.nue@gmail.com>
2019-08-09 15:56:23 +0200

    begin work on card home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,7 @@
   <div class="home">
     <img src="@/assets/img/Breguet_Atlantic.jpg" alt="Breguet Atlantic" width="100%" />
     <h1 class="text-center bg-primary">Bienvenue sur Atlantic</h1>
-    <div class="d-flex justify-content-center flex-wrap">
-      <b-button
-        v-for="plane in planes"
-        v-bind:key="plane.registration"
-        size="lg"
-        variant="primary"
-        v-bind:href="'/#/log3s/' + plane.registration"
-        class="m-1"
-      >{{ plane.registration }}</b-button>
-    </div>
+    <div class="d-flex justify-content-center flex-wrap"></div>
   </div>
 </template>
 
