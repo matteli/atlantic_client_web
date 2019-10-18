a941d27ae0dac5f48cd655c4248a48ce39bb73fc
matteli <matthieu.nue@gmail.com>
2019-10-18 18:27:53 +0200

    vue-cli v4 compatibility

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Log3S from '../views/Log3S.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/log3s/:registration',
+        name: 'log3s',
+        component: Log3S
+    },
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,28 @@
 module.exports = {
     publicPath: '/',
     pwa: {
+        name: "Atlantic",
+        theme_color: "#007bff",
+        appleMobileWebAppCapable: "yes",
+        manifestOptions: {
+            short_name: "Atlantic",
+            start_url: "./index.html",
+            display: "standalone",
+            background_color: "#000000",
+            orientation: "landscape",
+            icons: [
+                {
+                    "src": "./img/icons/android-chrome-192x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png"
+                },
+                {
+                    "src": "./img/icons/android-chrome-512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
+                }
+            ],
+        },
         workboxOptions: {
             skipWaiting: true,
             runtimeCaching: [
