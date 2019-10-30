7d211ed7cb2d8eb394556686e5eff2d30da2fbdd
matteli <matthieu.nue@gmail.com>
2019-10-30 10:28:20 +0100

    begin instructionsheet

---
+++ src/views/InstructionSheetDesigner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-form-input v-model="newInstruction.text" placeholder="Enter your instruction here"></b-form-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InstructionSheetDesigner",
+  data() {
+    return {
+      instructions: [],
+      newInstruction: {text=""}
+    };
+  }
+};
+</script>
+
+<style>
+</style>
